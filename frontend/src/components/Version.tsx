7b80f1aba741d3fdbe8131d9a0a2044f819f87ca
Chaz Winter <chas@hmrmusic.com>
2024-09-02 16:20:05 -0400

    Modified version number update logic

--- frontend/src/components/Version.tsx
+++ frontend/src/components/Version.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Version = () => {
-    const now = new Date();
-
-    const pad = (number: number) => number.toString().padStart(2, '0');
-
-    const version = `${now.getFullYear()}${pad(now.getMonth() + 1)}${pad(now.getDate())}-${pad(now.getHours())}${pad(now.getMinutes())}${pad(now.getSeconds())}`;
+    const version = process.env.REACT_APP_VERSION || 'Unknown Version';
 
     return (
         <div className='Version'>
