2b5e971c8da1fecc406e7ba2f252a0483fcaebdd
Chaz Winter <chas@hmrmusic.com>
2025-07-22 23:27:10 -0700

    Update rest api base URL

--- frontend/src/api/rest.ts
+++ frontend/src/api/rest.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/v1';
+const baseURL = process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api/v1` : 'http://localhost:8000/api/v1';
 
 const apiClient = axios.create({
   baseURL,
